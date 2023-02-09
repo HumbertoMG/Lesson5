fafc2444414dfbc8e3a388668d65c1bae08f58de
HumbertoMG <123428978+HumbertoMG@users.noreply.github.com>
2023-02-08 21:46:35 -0600

    Create Arreglosnumpy.py

---
+++ Arreglosnumpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+arreglo=np.array(3)
+for i in range(4,52): arreglo=np.append(arreglo,i)
+print (arreglo)
+
+
+arrsort = np.array([0.1.2.3.4.0.1.2.3.4])
+print("Arreglo original: ",arrsort)
+arrsort.sort()
+print("Arreglo ordenado: ",arrsort)
